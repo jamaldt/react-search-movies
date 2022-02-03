5ca7e38a0ea81413ee8fc82599738e6817fbdcf2
jamal <carldeltoro@gmail.com>
2022-02-02 20:34:18 -0400

    Create SearchForm Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import {Title} from './components/title'
+import {SearchForm} from './components/SearchForm'
+
 
 function App() {
   return (
     <div className="App">
       <Title>Search Movies</Title>
+      <div className='SearchForm-Wrapper'>
+      <SearchForm/>
+      </div>
+      
     </div>
   );
 }
