70541d0be676ff83956777752780009f19cb4860
jamal <carldeltoro@gmail.com>
2022-02-07 18:32:52 -0400

    Style in MoviesList component

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,24 +1,31 @@
-import React,{Component} from "react";
-import PropTypes from 'prop-types';
-import {Movie} from './Movie'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Movie } from './Movie'
 
-export class MoviesList extends Component{
-    static propTypes = {
-        movies: PropTypes.array
-    }
+export class MoviesList extends Component {
+  static propTypes = {
+    movies: PropTypes.array
+  }
 
-    render(){
-        const {movies} = this.props
-    return movies.map(movie => {
-      return(
-         <Movie 
-              key={movie.imdbID}
-              title={movie.Title}
-              year={movie.Year}
-              poster={movie.Poster}/>
-      )
-      //return <p key={movie.imdbID}>{movie.Title}</p>
-    })
-    }
+  render () {
+    const { movies } = this.props
+    return (
+      <div className='MoviesList'>
+        {
+          movies.map(movie => {
+            return (
+              <div key={movie.imdbID} className='MoviesList-item'>
+                <Movie
+                  id={movie.imdbID}
+                  title={movie.Title}
+                  year={movie.Year}
+                  poster={movie.Poster}
+                />
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
-
